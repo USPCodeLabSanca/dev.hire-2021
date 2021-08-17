913eab87f9730c3db7e8fc7ccf78919a83141810
Yure Pablo <48031838+yp1plus@users.noreply.github.com>
2021-08-17 10:50:33 -0300

    Update 226. Invert Binary Tree.cpp

--- 226. Invert Binary Tree.cpp
+++ 226. Invert Binary Tree.cpp
@@ -1,1 +1,31 @@
-
+class Solution {
+public:
+    //O(n) time -> number of nodes
+    //O(d) space -> depth
+    TreeNode* invertTree(TreeNode* root) {
+        if (root == NULL){
+            return NULL;
+        }
+        
+        traversePostOrder(root);
+        
+        return root;
+    }
+    
+    void traversePostOrder(TreeNode* root){
+        if (root == NULL){
+            return;
+        }
+         
+        traversePostOrder(root->left);
+        traversePostOrder(root->right);
+        swap(&(root->left), &(root->right));
+        
+    }
+    
+    void swap(TreeNode** left, TreeNode** right){
+        TreeNode* aux = *left;
+        *left = *right;
+        *right = aux;
+    }
+};
